196408199a375d8ba325b437af8c0c4ccf3ae7cd
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-08-19 11:42:57 +0600

    ellipse code added

--- Scripts/calculator.js
+++ Scripts/calculator.js
@@ -1,24 +1,24 @@
 
-function calculateTriangleArea(){
-//    get triangle base value
-const baseField = document.getElementById('traiangle-base');
-const baseValueText = baseField.value ;
-const base = parseFloat(baseValueText);
-console.log(base);
+function calculateTriangleArea() {
+    //    get triangle base value
+    const baseField = document.getElementById('traiangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText);
+    console.log(base);
 
-//
-const heightField = document.getElementById('traingle-height');
-const heightValueText = heightField.value;
-const height = parseFloat(heightValueText);
+    //
+    const heightField = document.getElementById('traingle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
 
-console.log(height);
-const area = 0.5*base*height;
-console.log(area);
-// 
-const areaSpan = document.getElementById('triangle-area') ;
-areaSpan.innerText = area;
+    console.log(height);
+    const area = 0.5 * base * height;
+    console.log(area);
+    // 
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
 }
-function calculateRectangleArea(){
+function calculateRectangleArea() {
     // console.log('rec');
     const widthField = document.getElementById('rectangle-width');
     const widthValueText = widthField.value;
@@ -30,32 +30,40 @@ function calculateRectangleArea(){
     const length = parseFloat(lengthValuetext);
     console.log(length);
     // 
-    const area = width*length;
+    const area = width * length;
     // 
     const rectangleAreaSpan = document.getElementById('rectangle-area');
     rectangleAreaSpan.innerText = area;
- 
-   
+
+
 }
- // reusable function reduce duplicate 
-function calculateParallelogramArea(){
-    const base  = getInputValue('parallelogram-base')
+// reusable function reduce duplicate 
+function calculateParallelogramArea() {
+    const base = getInputValue('parallelogram-base')
     console.log(base);
     const height = getInputValue('parallelogram-height');
     console.log(height);
-    const area = base*height;
-    setElementInnerText('parallelogram-area',area)
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area)
 }
 function getInputValue(fieldId) {
     const inputField = document.getElementById(fieldId);
     const inputValueText = inputField.value;
     const value = parseFloat(inputValueText);
     return value;
-    
+
 }
 // reusable set span ,p ,div , etc text
-function setElementInnerText(elementId,area){
+function setElementInnerText(elementId, area) {
     const element = document.getElementById(elementId);
     element.innerText = area;
 }
+// 
+function calculateEllipseArea(){
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+    const area = 3.14 *majorRadius * minorRadius;
+    setElementInnerText('ellipse-area',area);
+}
+
 
